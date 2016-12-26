e08ef4b43da6cb02100dc5c115509be0440e1817
Jokke <jonnejokke2@gmail.com>
2016-12-26 17:40:47 +0200

    Few bug fixes, IEnumerable<string> support added.
    When sending multiple files, with different SendFile() methods without Thread.Sleep() in the middle, it was putting every file contents to the last file. I changed threads to thread pools, so when operation is done thread is cancelled also. Also removed Close() methods since using() will dispose them. Changed string array on SendFiles method to IEnumerable<string>, so it can now send List/HashSet etc.. of files. And yeah, added additional slash to savepath IF it doesn't contain one on userinput.

--- SharpFileTransferrer/SharpClient.cs
+++ SharpFileTransferrer/SharpClient.cs
@@ -6,117 +6,138 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace SharpFileTransferrer
+namespace NahroTo.SharpFileTransferrer
 {
-    public class SharpClient
-    {
-        private const int MAX_PACKET_SIZE = 32768;
+	public class SharpClient
+	{
+		private const int MAX_PACKET_SIZE = 32768;
 
-        private class Address
-        {
-            public string ip;
-            public int port;
-            public string[] files;
+		private class Address : IDisposable
+		{
+			public string ip;
+			public int port;
+			public IEnumerable<string> files;
+			public CancellationTokenSource cts;
 
-            public Address(string ip, int port, string[] files)
-            {
-                this.ip = ip;
-                this.port = port;
-                this.files = files;
-            }
-        }
+			public Address(string ip, int port, IEnumerable<string> files)
+			{
+				this.ip = ip;
+				this.port = port;
+				this.files = files;
+				cts = new CancellationTokenSource();
+			}
 
-        public static void SendFile(string ipadress, int port, string filePath)
-        {
-            Thread thread = new Thread(new ParameterizedThreadStart(ConnectAsClient));
-            thread.Start(new Address(ipadress, port, new string[] { filePath }));
-        }
+			public void Dispose()
+			{
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
 
-        public static void SendFiles(string ipadress, int port, string[] filePaths)
-        {
-            Thread thread = new Thread(new ParameterizedThreadStart(ConnectAsClient));
-            thread.Start(new Address(ipadress, port, filePaths));
-        }
+			protected virtual void Dispose(bool disposing)
+			{
+				if (disposing && (cts != null))
+				{
+					cts.Cancel();
+					cts.Dispose();
+					cts = null;
+				}
+			}
+		}
+
+		public static void SendFile(string ipadress, int port, string filePath)
+		{
+			ThreadPool.QueueUserWorkItem(token =>
+			{
+				ConnectAsClient(new Address(ipadress, port, new string[] { filePath }));
+			});
+			Thread.Sleep(500);
+		}
+
+		public static void SendFiles(string ipadress, int port, IEnumerable<string> filePaths)
+		{
+			ThreadPool.QueueUserWorkItem(token =>
+			{
+				ConnectAsClient(new Address(ipadress, port, filePaths));
+			});
+			Thread.Sleep(500);
+		}
 
 
-        private static void ConnectAsClient(object address) // #3
-        {
-            Address param = (Address)address;
-            using (TcpClient client = new TcpClient())
-            {
-                client.Connect(IPAddress.Parse(param.ip), param.port);
-                using (NetworkStream stream = client.GetStream())
-                {
-                    bool newFile = true;
-                    stream.Write(BitConverter.GetBytes(param.files.Length), 0, 4);
-                    for (int i = 0; i < param.files.Length; i++)
-                    {
-                        string filePath = param.files[i];
-                        int fileLength = File.ReadAllBytes(filePath).Length;
-                        double amountFilePackets = 1; // one data info packet
-                        if (fileLength > MAX_PACKET_SIZE)
-                        {
-                            amountFilePackets = Math.Ceiling((double)fileLength / MAX_PACKET_SIZE);
-                        }
-                        if (newFile)
-                        {
-                            // WRITE PACKET 0 (FILE INFO)
-                            byte[] dataFilename = Encoding.ASCII.GetBytes(System.IO.Path.GetFileName(filePath));
-                            int packetLength0 = dataFilename.Length;
-                            // write packetlength0 (4 bytes)
-                            stream.Write(BitConverter.GetBytes(packetLength0), 0, 4);
-                            byte[] header0 = { 0 };
-                            // write header0 (1 byte)
-                            stream.Write(header0, 0, 1);
-                            // write dataFilename (packetlength0 bytes)
-                            stream.Write(dataFilename, 0, packetLength0);
+		private static void ConnectAsClient(Address param) // #3
+		{
+			using (TcpClient client = new TcpClient())
+			{
+				client.Connect(IPAddress.Parse(param.ip), param.port);
+				using (NetworkStream stream = client.GetStream())
+				{
+					bool newFile = true;
+					stream.Write(BitConverter.GetBytes(param.files.Count()), 0, 4);
+					for (int i = 0; i < param.files.Count(); i++)
+					{
+						string filePath = param.files.ElementAt(i);
+						int fileLength = File.ReadAllBytes(filePath).Length;
+						double amountFilePackets = 1; // one data info packet
+						if (fileLength > MAX_PACKET_SIZE)
+						{
+							amountFilePackets = Math.Ceiling((double)fileLength / MAX_PACKET_SIZE);
+						}
+						if (newFile)
+						{
+							// WRITE PACKET 0 (FILE INFO)
+							byte[] dataFilename = Encoding.ASCII.GetBytes(System.IO.Path.GetFileName(filePath));
+							int packetLength0 = dataFilename.Length;
+							// write packetlength0 (4 bytes)
+							stream.Write(BitConverter.GetBytes(packetLength0), 0, 4);
+							byte[] header0 = { 0 };
+							// write header0 (1 byte)
+							stream.Write(header0, 0, 1);
+							// write dataFilename (packetlength0 bytes)
+							stream.Write(dataFilename, 0, packetLength0);
 
-                            newFile = false;
-                        }
-                        // WRITE PACKET 1 (FILE DATA)
-                        int packetsLeftToWrite = (int)amountFilePackets;
-                        int packetsWritten = 0;
-                        int remainingBytes;
-                        while (packetsLeftToWrite != 0)
-                        {
-                            byte[] dataFilePacket;
-                            remainingBytes = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).ToArray().Length;
-                            if (!(remainingBytes <= MAX_PACKET_SIZE)) // if remainig bytes are NOT enough for 1 packet
-                            {
-                                dataFilePacket = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).Take(MAX_PACKET_SIZE).ToArray();
-                            }
-                            else // if last packet
-                            {
-                                dataFilePacket = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).Take(File.ReadAllBytes(filePath).Length - packetsWritten * MAX_PACKET_SIZE).ToArray();
-                                newFile = true;
-                            }
-                            int packetLength1 = dataFilePacket.Length;
-                            // write packetlength1 (4 bytes)
-                            stream.Write(BitConverter.GetBytes(packetLength1), 0, 4);
-                            packetsLeftToWrite--;
-                            byte[] header = new byte[1];
-                            if (packetsLeftToWrite == 0)
-                            {
-                                header[0] = 2;
-                            }
-                            else
-                            {
-                                header[0] = 1;
-                            }
-                            // write header1 (1 byte)
-                            stream.Write(header, 0, 1);
+							newFile = false;
+						}
+						// WRITE PACKET 1 (FILE DATA)
+						int packetsLeftToWrite = (int)amountFilePackets;
+						int packetsWritten = 0;
+						int remainingBytes;
+						while (packetsLeftToWrite != 0)
+						{
+							byte[] dataFilePacket;
+							remainingBytes = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).ToArray().Length;
+							if (!(remainingBytes <= MAX_PACKET_SIZE)) // if remainig bytes are NOT enough for 1 packet
+							{
+								dataFilePacket = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).Take(MAX_PACKET_SIZE).ToArray();
+							}
+							else // if last packet
+							{
+								dataFilePacket = File.ReadAllBytes(filePath).Skip(packetsWritten * MAX_PACKET_SIZE).Take(File.ReadAllBytes(filePath).Length - packetsWritten * MAX_PACKET_SIZE).ToArray();
+								newFile = true;
+							}
+							int packetLength1 = dataFilePacket.Length;
+							// write packetlength1 (4 bytes)
+							stream.Write(BitConverter.GetBytes(packetLength1), 0, 4);
+							packetsLeftToWrite--;
+							byte[] header = new byte[1];
+							if (packetsLeftToWrite == 0)
+							{
+								header[0] = 2;
+							}
+							else
+							{
+								header[0] = 1;
+							}
+							// write header1 (1 byte)
+							stream.Write(header, 0, 1);
 
-                            // write dataFilePacket
-                            stream.Write(dataFilePacket, 0, packetLength1);
-                            packetsWritten++;
-                        }
-                    }
-                    stream.Close();
-                }
-                client.Close();
-            }
-        }
-    }
+							// write dataFilePacket
+							stream.Write(dataFilePacket, 0, packetLength1);
+							packetsWritten++;
+						}
+					}
+				}
+			}
+			param.Dispose();
+		}
+	}
 }
